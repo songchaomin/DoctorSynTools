45de9f5e9bfe5e58384ead0e9119c37d44f53218
songchaomin <songchaomin@126.com>
2020-12-22 16:31:50 +0800

    增加消毁的监听

---
+++ src/main/java/com/kuka/listener/DestoryListener.java
@@ -0,0 +1,56 @@
+package com.kuka.listener;
+
+import com.kuka.utils.MailUtils;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Component
+public class DestoryListener implements DisposableBean {
+    @Override
+    public void destroy() throws Exception {
+        String ip = "";
+        String chinaz = "http://ip.chinaz.com";
+        StringBuilder inputLine = new StringBuilder();
+        String read = "";
+        URL url = null;
+        HttpURLConnection urlConnection = null;
+        BufferedReader in = null;
+        try {
+            url = new URL(chinaz);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            in = new BufferedReader( new InputStreamReader(urlConnection.getInputStream(),"UTF-8"));
+            while((read=in.readLine())!=null){
+                inputLine.append(read+"\r\n");
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            if(in!=null){
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+        Pattern p = Pattern.compile("\\<dd class\\=\"fz24\">(.*?)\\<\\/dd>");
+        Matcher m = p.matcher(inputLine.toString());
+        if(m.find()){
+            String ipstr = m.group(1);
+            ip = ipstr;
+            MailUtils.sendMail("华源库存同步远程服务关闭","库存同步IP地址："+ip);
+        }
+    }
+}
